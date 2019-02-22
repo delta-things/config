066d73e61044371e2fdf4ed6aac5ae092f28feb9
Delta Things <45587410+delta-things@users.noreply.github.com>
2019-02-22 18:21:47 +0530

    set_idf_path.sh created

---
+++ set_idf_path.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Please select ESP platform ";
+echo "1.ESP322";
+echo "2.ESP8266";
+echo -n "Enter your choice (1 or 2 ?) : "; 
+
+read PLATFORM
+
+case "$PLATFORM" in
+  "1") 
+	echo "Exporting IDF_PATH for ESP32"
+	export IDF_PATH="${HOME}/esp/esp-idf";;
+  "2") 
+	echo "Exporting IDF_PATH for ESP8266"
+	export IDF_PATH="${HOME}/esp/ESP8266_RTOS_SDK";;
+  *) 
+	echo "Error : Not a valid platform";;
+esac
+
+echo -n "IDF_PATH = "
+printenv IDF_PATH
