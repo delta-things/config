bcb1562fdd360fd95b684d062d54cf6bae50a2c1
Delta <deltathings@gmail.com>
2019-02-27 17:08:44 +0530

    Bug fixed

--- set_idf_path.sh
+++ set_idf_path.sh
@@ -6,17 +6,24 @@ echo "2.ESP8266";
 echo -n "Enter your choice (1 or 2 ?) : "; 
 
 read PLATFORM
-
+path_set=false
 case "$PLATFORM" in
   "1") 
 	echo "Exporting IDF_PATH for ESP32"
-	export IDF_PATH="${HOME}/esp/esp-idf";;
+	export IDF_PATH="${HOME}/esp/esp-idf"
+	path_set=true;;
   "2") 
 	echo "Exporting IDF_PATH for ESP8266"
-	export IDF_PATH="${HOME}/esp/ESP8266_RTOS_SDK";;
+	export IDF_PATH="${HOME}/esp/ESP8266_RTOS_SDK"
+	path_set=true;;
   *) 
-	echo "Error : Not a valid platform";;
+	echo "Error : Not a valid platform"
+	echo "Warning : IDF_PATH is not set..!"
+	echo "Please select valid platform";;
 esac
 
-echo -n "IDF_PATH = "
-printenv IDF_PATH
+if $path_set == true
+then
+	echo -n "IDF_PATH = "
+	printenv IDF_PATH
+fi
